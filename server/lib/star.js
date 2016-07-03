8be68436e9ff9660a5982544741e4e1d0a718aff
Soldy <testa.a.tapos@gmail.com>
2016-07-03 16:21:24 +0000

    add star

---
+++ server/lib/star.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var mLstar = function(){
+    this.insideThings={
+        ligthPower : 0,
+        size : 0,
+    }
+    this.turnEvents = function () {
+        
+    }
+    this.init = function() {
+        
+    }
+    this.reload = function() {
+        
+    }
+    this.save = function () {
+        
+    }
+}
+
+
