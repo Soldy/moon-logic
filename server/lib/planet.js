cb72078dc8f1a8cb5013c31558b5c8efc5a8dbef
Soldy <testa.a.tapos@gmail.com>
2016-07-03 16:30:38 +0000

    change planet logic

--- server/lib/planet.js
+++ server/lib/planet.js
@@ -1,28 +1,30 @@
 
 
 
-var mLplanet = function (){
-    this.buildings = [];
-    this.satelits = [];
-    this.terraFormated = 0;
-    this.water = 0;
-    this.air = 0;
-    this.lifemore = 0;
-    this.mass = 0;
-    this.buildingSpace = 0;
-    this.coal = 0;
-    this.iron = 0;
-    this.titan = 0;
-    this.radium = 0;
-    this.polonium = 0;
-    this.zedium = 0;
-    this.oil = 0;
-    this.gas=0;
-    this.animals=0;
-    this.plant=0;
-    this.posion=0;
+var mLplanet = function (){    
+    this.insideThings={
+        buildings:[],
+        satelits:[],
+        minerals:{},
+        plans:[],
+        animals:[],
+        microbe:[],
+        radioactivity:0,
+        mass:0,
+        size:0,
+        position:0,
+        terraFormated:0,
+    }
     this.turnEvent=function(){
 
-
+    }
+    this.init = function () {
+        
+    }
+    this.reload = function () {
+        
+    }
+    this.save = function () {
+        
     }
 }
